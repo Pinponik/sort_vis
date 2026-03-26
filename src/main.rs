5b3d3db5a3639216342f9b2c1f52a044b29699cf
Pinponik <256419204+Pinponik@users.noreply.github.com>
2026-03-26 21:53:31 +0100

    Update.
    > [!WARNING]
    > Do not compile.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,105 @@
-use ratatui::style::Stylize;
-use ratatui::widgets::{Block, Paragraph};
+use std::io;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ratatui::run(|terminal| {
-        let list: Vec<i64> = vec![0, 3, 25, 1, 237, 2];
-        terminal.draw(|frame| {
-            let block = Block::bordered().title("Sorting...");
-            let greeting = Paragraph::new(
-                list.iter()
-                    .map(|x| x.to_string())
-                    .collect::<Vec<String>>()
-                    .join(", "),
-            )
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
+
+fn main() -> io::Result<()> {
+    ratatui::run(|terminal| App::default().run(terminal))
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    list: Vec<u64>,
+    change: (usize, usize),
+    read: Vec<usize>,
+    exit: bool,
+}
+
+impl App {}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Counter App Tutorial ".bold());
+        let instructions = Line::from(vec![
+            " Quicksort ".into(),
+            "<q>".blue().into(),
+            " Exit ".into(),
+            " <e> ".red().into(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let nums_text = Text::from(vec![Line::from(
+            self.list
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>(),
+        )]);
+
+        Paragraph::new(nums_text)
             .centered()
-            .yellow()
-            .block(block);
-            frame.render_widget(greeting, frame.area());
-        })?;
-        std::thread::sleep(std::time::Duration::from_secs(5));
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+impl App {
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+}
+
+impl App {
+    /// updates the application's state based on user input
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
         Ok(())
-    })
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('e') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.list.push(rand.gen_range(0..=999));
+    }
+
+    fn decrement_counter(&mut self) {
+        self.list.pop();
+    }
+
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
 }
